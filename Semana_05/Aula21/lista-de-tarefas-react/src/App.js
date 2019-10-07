1756f1a2b4a8f3a1a1f2702413393a8bde0c45ae
Samuel <squoliver@gmail.com>
2019-10-07 16:52:19 -0300

    ainda muito ruim, 'explodindo' ao clicar no botão '+'

--- Semana_05/Aula21/lista-de-tarefas-react/src/App.js
+++ Semana_05/Aula21/lista-de-tarefas-react/src/App.js
@@ -1,26 +1,74 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import {Tarefa} from './Components/Tarefa/Tarefa';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const BigContainer = styled.div`
+  width: 30%;
+  margin: 100px auto;
+  background-color: beige;  
+`
+
+const Cabecalho = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0 25px;
+`
+
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      listaTarefas: [],
+    }
+  }
+
+  criarCampoTexto = () => {
+    console.log("fui clicado");
+
+    //criando a nova tarefa:
+    const novaTarefa = {
+      descricao: "teste", //???
+      feita: false
+    }
+
+    //atualizando a lista com a nova tarefa:
+    const listaAtualizada = this.state.listaTarefas;
+    listaAtualizada.push(novaTarefa);
+    this.setState({listaTarefas: listaAtualizada});
+  }
+
+  render(){
+
+    const completas = this.state.listaTarefas.filter((elem) => {
+      return (elem.feita === true);
+    });
+
+    const incompletas = this.state.listaTarefas.filter((elem) => {
+      return (elem.feita === false);
+    });
+
+    return (
+      <BigContainer>
+        <p>TAREFAS</p>
+        <Cabecalho>
+          <span>A fazer:</span>
+          <button onClick={this.criarCampoTexto}>+</button>
+        </Cabecalho>
+        <ul>
+          {completas}
+          {<Tarefa descTarefa={"teste"}></Tarefa>}
+        </ul>
+        <Cabecalho>
+          <span>Feitas:</span>
+        </Cabecalho>
+        <ul>
+          {incompletas}
+        </ul>
+      </BigContainer>
+    );
+  }
 }
 
 export default App;
