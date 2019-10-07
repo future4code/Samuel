36c2ad30ba8d397a4f271467666d518ea7f70f84
Samuel <squoliver@gmail.com>
2019-10-07 13:07:27 -0300

    Challenge - Aula 21 - OK

--- Semana_05/Aula21/challenge-aula21/src/App.js
+++ Semana_05/Aula21/challenge-aula21/src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { CaixaPreta } from './Components/CaixaPreta/CaixaPreta';
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const SmallContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 100vw;
+`
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      numeroDeBlocos: 1,
+    }
+  }
+
+  aoMudarNumeroBlocos = (event) => {
+    const numeroAtualDeBlocos = event.target.value;
+    this.setState({numeroDeBlocos: numeroAtualDeBlocos});
+  }
+
+  render(){
+
+    let lista = [];
+
+    for(let i = 0; i < this.state.numeroDeBlocos; i++){
+      lista.push(<CaixaPreta key={i} />);
+    }
+
+    return (
+      <div>
+        <input 
+          type="number"
+          value={this.state.numeroDeBlocos}
+          onChange={this.aoMudarNumeroBlocos}
+        />
+        <hr/>
+        <SmallContainer> {lista} </SmallContainer>
+      </div>
+    );
+  }
 }
 
 export default App;
