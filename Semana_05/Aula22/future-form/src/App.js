a10bcca40927e57647661520c483f053912fb744
Samuel <squoliver@gmail.com>
2019-10-08 15:12:12 -0300

    Update - FutureForms...

--- Semana_05/Aula22/future-form/src/App.js
+++ Semana_05/Aula22/future-form/src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import EtapaUm from './Components/EtapaUm/EtapaUm';
+import EtapaDois from './Components/EtapaDois/EtapaDois';
+import EtapaTres from './Components/EtapaTres/EtapaTres';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const BotaoEnviar = styled.button`
+  width: 100px;
+  margin: 20px;
+  padding: 10px;
+  border: 2px solid;
+  border-radius: 5px;
+  color: white;
+  background-color: orange;
+  &:active{
+    background-color: blue;
+  }
+`
+
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  aoEnviarFormulario = () => {
+    console.log("fui clicado");
+  }
+
+  render(){
+    return (
+      <Container>
+        <EtapaUm />
+        <EtapaDois />
+        <EtapaTres />
+        <BotaoEnviar onClick={this.aoEnviarFormulario}>Enviar</BotaoEnviar>
+      </Container>
+    );
+  }
 }
 
 export default App;
